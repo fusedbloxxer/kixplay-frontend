0d3e2c7ac4abc632b62e691e17722377c7521c8b
Andrei Ionescu <voidwarden@yahoo.com>
2022-01-24 13:01:56 +0200

    Add navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { UserLoginComponent } from './components/toolbar/user-login/user-login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { NavigationComponent } from './components/toolbar/navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ToolbarComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    UserLoginComponent,
+    NavigationComponent,
+  ],
   imports: [
+    // Common Modules
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     HttpClientModule,
+
+    // Custom Modules
+    AppRoutingModule,
+
+    // Material UI
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
